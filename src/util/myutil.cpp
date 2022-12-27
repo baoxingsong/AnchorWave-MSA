bc7d8429f9f75aa5ffc2529d80fbd347a5126644
sugelan0536 <zhou_yihua@163.com>
2022-12-27 16:39:15 +0800

    remove regex

--- src/util/myutil.cpp
+++ src/util/myutil.cpp
@@ -26,7 +26,7 @@
 
 #endif
 
-void split(const std::string &s, char &delim, std::vector<std::string> &elems) {
+void split(const std::string &s, char &delim, std::vector <std::string> &elems) {
     std::stringstream ss(s);
     std::string item;
     while (std::getline(ss, item, delim)) {
@@ -50,11 +50,15 @@ int32_t min(const int32_t &a, const int32_t &b) {
     return b;
 }
 
-void splitCIGAR(std::string cigarString, std::vector<std::string> &cigarElems) {
-    std::regex reg("([0-9]+[MIDNSHPX=])");
-    std::smatch match;
-    while (regex_search(cigarString, match, reg)) {
-        cigarElems.push_back(match[1]);
-        cigarString = match.suffix().str();
+void splitCIGAR(std::string str, std::vector <std::string> &cigarElems) {
+    while (str.length() > 0) {
+        int p_d = str.find_first_of("1234567890");
+        int p_c = str.find_first_of("MIDNSHPX=");
+
+        if(p_c > p_d) {
+            std::string s = str.substr(p_d, p_c + 1);
+            cigarElems.push_back(s);
+        }
+        str = str.substr(p_c + 1);
     }
 }
